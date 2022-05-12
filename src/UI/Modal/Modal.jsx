1dca4460bc4067415caa7d1daa5f7e9518ad2d61
igor <igor.homechko@gmail.com>
2022-05-12 17:49:27 +0700

    all

--- src/UI/Modal/Modal.jsx
+++ src/UI/Modal/Modal.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import cl from './Modal.module.css'
+// import { useMedia } from 'react-media'
 
+// const GLOBAL_MEDIA_QUERIES = {
+//   small: "(max-width: 599px)",
+//   medium: "(min-width: 600px) and (max-width: 1199px)",
+//   large: "(min-width: 1200px)"
+// }
+// const matches = useMedia({ queries: GLOBAL_MEDIA_QUERIES })
+
+// const marginBottom = matches.large ? 0 : 10
 
 const Modal = ({children, visible, setVisible}) => {
     const addClasses = [cl.modal]
